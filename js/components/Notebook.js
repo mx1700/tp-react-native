8fa80c8ec3452871396464d4902bef605fd8b55c
mx1700 <mx1700@gmail.com>
2016-09-11 14:38:17 +0800

    修改日记本样式

--- js/components/Notebook.js
+++ js/components/Notebook.js
@@ -24,21 +24,29 @@ function bookView(props) {
         <TouchableOpacity key={book.id} onPress={props.onPress} style={props.style}>
             <View style={{
                 width: 140,
-                shadowColor: '#000',
+                shadowColor: '#444',
                 shadowOpacity: 0.1,
                 shadowOffset: { width: 0, height: 0 },
                 backgroundColor: '#fff',
                 alignItems:'center',
-                paddingBottom: 5,
             }}>
                 <Image key={book.id} style={{width: 140, height: 105, flexDirection: 'row', justifyContent: 'flex-end'}} source={{uri: book.coverUrl}}>
                     {label}
                 </Image>
-                <View style={{alignItems: 'center', justifyContent: 'center', padding: 5, height: 55}}>
-                    <Text style={{textAlign: 'center', fontWeight: 'bold', color: TPColors.contentText}}>{book.subject}</Text>
+                <View style={{
+                    alignItems:'center',
+                    width: 141,
+                    borderColor: '#eee',
+                    borderWidth: StyleSheet.hairlineWidth,
+                    borderTopWidth: 0,
+                    paddingBottom: 5,
+                }}>
+                    <View style={{alignItems: 'center', justifyContent: 'center', padding: 5, height: 55}}>
+                        <Text style={{textAlign: 'center', fontWeight: 'bold', color: TPColors.contentText}}>{book.subject}</Text>
+                    </View>
+                    <Text style={{ fontSize: 10, color: TPColors.inactiveText}}>{exp}</Text>
+                    <Text style={{ fontSize: 10, color: TPColors.inactiveText}}>{book.created}至{book.expired}</Text>
                 </View>
-                <Text style={{ fontSize: 10, color: TPColors.inactiveText}}>{exp}</Text>
-                <Text style={{ fontSize: 10, color: TPColors.inactiveText}}>{book.created}至{book.expired}</Text>
             </View>
         </TouchableOpacity>
     )
